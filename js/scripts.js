14337647a92ce02b7ed3c6fc5cbf1de7b916ba06
Chris Davis <cdavis@transtechsys.com>
2017-03-02 13:43:34 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,68 @@
+
+$(function() {
+
+
+
+$('#fullpage').fullpage({
+	anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage']
+});
+
+
+
+
+	// var myFunctions = {
+	// 	init: function() {
+	// 		this.alertSomething();
+	// 		this.consoleLogSomething();
+	// 		this.appendSomething();
+	// 	},
+
+	// 	alertSomething: function() {
+	// 		alert("Hi Chris");
+	// 	},
+
+	// 	consoleLogSomething: function() {
+	// 		console.log("Console logged after alert");
+	// 	},
+
+	// 	appendSomething: function() {
+	// 		$(".container").append("<p>Hi There</p>");
+	// 	}
+	// }
+
+	// myFunctions.init();
+
+	
+	// $.getJSON("http://codepen.io/anon/pen/dNgjva.js", function(data) {
+	// 	for(var i = 0; i < data.movies.length; i++) {
+	// 		$(".container").append("<div class='images'><p>Title</p><h2>" + data.movies[i].movieTitle + "</h2><p>Director</p><h3>Director: " + data.movies[i].movieDirector + "</h3><img src='" + data.movies[i].movieImage + "'></div>");
+	// 	}
+	// });
+
+	
+
+	// var data = {
+	// 	movies: [
+	// 		{
+	// 			movieTitle: "Never Go Back",
+	// 			movieDirector: "Gavin Hood",
+	// 			movieImage: "http://images.redbox.com/Images/EPC/boxartvertical/200097.jpg"
+	// 		},
+	// 		{
+	// 			movieTitle: "Max Steel",
+	// 			movieDirector: "Bill Condon",
+	// 			movieImage: "http://images.redbox.com/Images/EPC/boxartvertical/200278.jpg"
+	// 		},
+	// 		{
+	// 			movieTitle: "Keeping Up with the Jonses",
+	// 			movieDirector: "Mikael Hafstrom",
+	// 			movieImage: "http://images.redbox.com/Images/EPC/boxartvertical/200107.jpg"
+	// 		}
+	// 	]
+	// };
+
+	
+
+});
+
+
